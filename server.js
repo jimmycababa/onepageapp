351f0ce3d342e79b5eff38545005496c4f84420e
jimmycababa <jimmycababa@gmail.com>
2021-06-19 14:23:46 -0500

    added static middleware functionality

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const path = require('path')
 
 const app = express();
 
+// whenever the path has /static, we are going to serve the static diredtory as usual
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve("frontend", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
 app.listen(process.env.PORT || 2121, () => console.log("Serving is doing it's thang!"));
