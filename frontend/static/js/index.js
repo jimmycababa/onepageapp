af009c85302a368082d861fae797bc9fecf1276f
jimmycababa <jimmycababa@gmail.com>
2021-06-20 17:47:42 -0500

    added history api

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,3 +1,10 @@
+// using the history API, redirecting to a page without loading a new resource
+// we are going to call the client side router to process the new history entry
+// this function will make it so when the user clicks on any of the links it will not reload the page and run the navigateTo function
+const navigateTo = url => {
+    history.pushState(null, null, url)
+    router()
+}
 // we are loading the content for each one of our views (dashboard, settings, pr posts). 
 const router = async () => {
     const routes = [
@@ -30,7 +37,17 @@ const router = async () => {
     console.log(match.route.view())
 };
 
+// we are listening for the popstate event
+window.addEventListener("popstate", router)
+
 // once all of the DOM has rendered, we run the router function
 document.addEventListener("DOMContentLoaded", () => {
+    // this is a delegated event listener. this makes the links work. grab the event object, if the target matches the selector of data-link, preventdefault will prevent the default behaviour of actually following the link and navigate to the actual href that is set on the element from the index.html page.
+    document.body.addEventListener("click", e => {
+        if(e.target.matches("[data-link")) {
+            e.preventDefault()
+            navigateTo(e.target.href)
+        }
+    })
     router();
 })
